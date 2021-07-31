6fae162175c30e5d66578aa4a5be9ba3c18f8b14
Lance Newman <lnewman@brightidea.com>
2021-07-31 12:36:27 -0400

    prod build

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -49,7 +49,7 @@
 
         <!-- React build CSS -->
         <link rel="stylesheet" type="text/css" href="/static/css/2.3daa458e.chunk.css">
-        <link rel="stylesheet" type="text/css" href="/static/css/main.5b6e5c75.chunk.css">
+        <link rel="stylesheet" type="text/css" href="/static/css/main.59872ccd.chunk.css">
 
         <title>{{ $title }}</title>
     </head>
@@ -69,7 +69,7 @@
     <script src="https://www.statcounter.com/counter/counter.js" async></script>
 
     <!-- React build JS -->
-    <script src="/static/js/2.917aa186.chunk.js"></script>
-    <script src="/static/js/main.f5c9ea25.chunk.js"></script>
+    <script src="/static/js/2.5138f41b.chunk.js"></script>
+    <script src="/static/js/main.07103968.chunk.js"></script>
     <script src="/static/js/runtime-main.e1b51eb3.js"></script>
 </html>
