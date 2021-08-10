bc6f87e1773639a3eff218cbd51bf4280805a80a
Lance Newman <lnewman@brightidea.com>
2021-08-10 14:18:50 -0400

    prod build

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -49,7 +49,7 @@
 
         <!-- React build CSS -->
         <link rel="stylesheet" type="text/css" href="/static/css/2.e4989b0c.chunk.css">
-        <link rel="stylesheet" type="text/css" href="/static/css/main.7d0154cc.chunk.css">
+        <link rel="stylesheet" type="text/css" href="/static/css/main.e096252c.chunk.css">
 
         <title>{{ $title }}</title>
     </head>
@@ -70,6 +70,6 @@
 
     <!-- React build JS -->
     <script src="/static/js/2.6616c8bb.chunk.js"></script>
-    <script src="/static/js/main.65cefd74.chunk.js"></script>
+    <script src="/static/js/main.32ea970f.chunk.js"></script>
     <script src="/static/js/runtime-main.e1b51eb3.js"></script>
 </html>
