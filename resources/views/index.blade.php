2a17bd872966d6d277b39df85ac0ff1d40491d6b
Lance Newman <lnewman@brightidea.com>
2021-08-10 15:13:10 -0400

    prod build

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -70,6 +70,6 @@
 
     <!-- React build JS -->
     <script src="/static/js/2.6616c8bb.chunk.js"></script>
-    <script src="/static/js/main.733bdee0.chunk.js"></script>
+    <script src="/static/js/main.2a52c764.chunk.js"></script>
     <script src="/static/js/runtime-main.e1b51eb3.js"></script>
 </html>
